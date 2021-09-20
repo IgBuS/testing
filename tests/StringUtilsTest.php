d5c83dd5e2e9e851aad32f2a16de982e0e657fb8
IgBuS <i.b@msulab.ru>
2021-09-20 17:05:29 +0300

    1

---
+++ tests/StringUtilsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Testing\StringUtils;
+
+if (StringUtils\capitalize('hello') !== 'Hello') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+if (StringUtils\capitalize('') !== '') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+echo 'Все тесты пройдены!';
